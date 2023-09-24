02bf68ae812425ff86b4aee9e099c096dd3789c5
Gaurav <gauravsomani52750@gmail.com>
2023-09-24 09:19:57 +0530

    few basic types

--- Basics/app.js
+++ Basics/app.js
@@ -1,1 +1,13 @@
-console.log("Hello, TypeScript!");
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+// let num1 = 5;
+// const num2 = 2.8;
+// let ans = add(num1, num2);
+// console.log(ans);
+// *Objects
+var person = {
+    name: "Gaurav",
+    age: 20
+};
+console.log(person.name);
