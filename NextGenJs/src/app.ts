72f11cb4671f2a7dbc0db56a35b28216f020dee4
Gaurav <gauravsomani52750@gmail.com>
2023-09-24 19:18:57 +0530

    revisiting es6 concepts

--- NextGenJs/src/app.ts
+++ NextGenJs/src/app.ts
@@ -1,1 +1,46 @@
-console.log("Next Gen JS!");
+console.log("Next Gen JS is ES6!");
+
+//* Arrow functions
+//default argument
+const add = (a: number, b: number = 1) => {
+  return a + b;
+};
+
+console.log(add(2));
+
+//Spread operator
+const hobbies = ["Sports", "Cooking", "Cricket"];
+// const activeHobbies = ["Anime", ...hobbies];
+
+const activeHobbies = ["Anime"];
+activeHobbies.push(...hobbies);
+
+console.log(activeHobbies);
+
+//spread operator with objects
+const person = {
+  firstName: "Gaurav",
+  age: 20,
+};
+
+const copiedPerson = { ...person, isSingle: true };
+console.log(copiedPerson);
+
+//Rest parameters
+
+const addNumbers = (...nums: number[]) => {
+  let res = 0;
+  for (const num of nums) {
+    res += num;
+  }
+  return res;
+};
+
+console.log(addNumbers(2, 3, 4, 5.6, 11));
+
+//Array and Object Destructuring
+const [hobby1, hobby2, ...remainingHobbies] = activeHobbies;
+console.log(hobby1, hobby2, remainingHobbies);
+
+const { firstName, age: myAge } = person;
+console.log(firstName, myAge);
