cccd66f33cc1a38b7c4da51c97dfc25f9dff8940
Gaurav <gauravsomani52750@gmail.com>
2023-09-25 11:28:19 +0530

    inheritance

--- ClassesInterfaces/dist/app.js
+++ ClassesInterfaces/dist/app.js
@@ -24,6 +24,13 @@ class Department {
         console.log(this.employees);
     }
 }
+//Inheritance
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "IT"); //super must be called first, it'll call the constructor of the base class
+        this.admins = admins;
+    }
+}
 const accountsDepartment = new Department("1A1", "Accounts");
 console.log(accountsDepartment);
 accountsDepartment.addEmployee("Max");
@@ -33,3 +40,50 @@ accountsDepartment.describe();
 // const acCopy = { name: "Dummy", describe: accountsDepartment.describe };
 // acCopy.describe();
 accountsDepartment.printEmployeeInfo();
+const ItDept1 = new ITDepartment("1A2", ["Admin_01"]);
+ItDept1.describe();
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    //getter
+    get getLastReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found!");
+    }
+    //setter
+    set setLastReport(value) {
+        if (value) {
+            this.addReport(value);
+            return;
+        }
+        throw new Error("Please pass in a valid value!");
+    }
+    addReport(text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+    getReports() {
+        console.log(this.reports);
+    }
+    //we can also override the base class methods
+    addEmployee(employeeName) {
+        if (employeeName === "Maxi") {
+            return;
+        }
+        this.employees.push(employeeName);
+    }
+}
+const accountsDept = new AccountingDepartment("1A3", ["report1", "report2"]); //[] is the reports
+accountsDept.addReport("report3");
+accountsDept.getReports();
+accountsDept.describe();
+console.log(accountsDept);
+accountsDept.addEmployee("Max");
+accountsDept.printEmployeeInfo();
+accountsDept.setLastReport = "report4 latest one";
+console.log(accountsDept.getLastReport);
--- ClassesInterfaces/src/app.ts
+++ ClassesInterfaces/src/app.ts
@@ -4,7 +4,7 @@ class Department {
   //   public name: string;
 
   //private field
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -27,6 +27,16 @@ class Department {
   }
 }
 
+//Inheritance
+class ITDepartment extends Department {
+  admins: string[];
+
+  constructor(id: string, admins: string[]) {
+    super(id, "IT"); //super must be called first, it'll call the constructor of the base class
+    this.admins = admins;
+  }
+}
+
 const accountsDepartment = new Department("1A1", "Accounts");
 console.log(accountsDepartment);
 accountsDepartment.addEmployee("Max");
@@ -40,3 +50,61 @@ accountsDepartment.describe();
 // acCopy.describe();
 
 accountsDepartment.printEmployeeInfo();
+
+const ItDept1 = new ITDepartment("1A2", ["Admin_01"]);
+ItDept1.describe();
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  //getter
+  get getLastReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found!");
+  }
+
+  //setter
+  set setLastReport(value: string) {
+    if (value) {
+      this.addReport(value);
+      return;
+    }
+    throw new Error("Please pass in a valid value!");
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  getReports() {
+    console.log(this.reports);
+  }
+
+  //we can also override the base class methods
+  addEmployee(employeeName: string): void {
+    if (employeeName === "Maxi") {
+      return;
+    }
+    this.employees.push(employeeName);
+  }
+}
+
+const accountsDept = new AccountingDepartment("1A3", ["report1", "report2"]); //[] is the reports
+accountsDept.addReport("report3");
+accountsDept.getReports();
+accountsDept.describe();
+console.log(accountsDept);
+
+accountsDept.addEmployee("Max");
+accountsDept.printEmployeeInfo();
+
+accountsDept.setLastReport = "report4 latest one";
+console.log(accountsDept.getLastReport);
