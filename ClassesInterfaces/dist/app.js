fcba3b5ca85d0b8641a6ad236ff38c82acc1704a
Gaurav <gauravsomani52750@gmail.com>
2023-09-27 18:36:57 +0530

    static methods, singleton class, abstract class

--- ClassesInterfaces/dist/app.js
+++ ClassesInterfaces/dist/app.js
@@ -10,11 +10,15 @@ class Department {
         this.employees = [];
         // this.id = id;
         // this.name = n;
+        // year = 2024;
+    }
+    //static method
+    static createEmployee(name) {
+        return { name: name };
     }
     //DUMMY PARAMETER
-    describe() {
-        console.log(`Department details: ${this.id} ${this.name}`);
-    }
+    // console.log(`Department details: ${this.id} ${this.name}`);
+    // }
     addEmployee(employee) {
         //some validation etc
         this.employees.push(employee);
@@ -24,22 +28,27 @@ class Department {
         console.log(this.employees);
     }
 }
+//static field
+Department.year = 2023;
 //Inheritance
 class ITDepartment extends Department {
     constructor(id, admins) {
         super(id, "IT"); //super must be called first, it'll call the constructor of the base class
         this.admins = admins;
     }
+    describe() {
+        console.log("IT Department - ID: " + this.id);
+    }
 }
-const accountsDepartment = new Department("1A1", "Accounts");
-console.log(accountsDepartment);
-accountsDepartment.addEmployee("Max");
-accountsDepartment.addEmployee("Maxi");
+// const accountsDepartment = new Department("1A1", "Accounts");
+// console.log(accountsDepartment);
+// accountsDepartment.addEmployee("Max");
+// accountsDepartment.addEmployee("Maxi");
 // accountsDepartment.employees.push("Anna");
-accountsDepartment.describe();
+// accountsDepartment.describe();
 // const acCopy = { name: "Dummy", describe: accountsDepartment.describe };
 // acCopy.describe();
-accountsDepartment.printEmployeeInfo();
+// accountsDepartment.printEmployeeInfo();
 const ItDept1 = new ITDepartment("1A2", ["Admin_01"]);
 ItDept1.describe();
 class AccountingDepartment extends Department {
@@ -55,6 +64,9 @@ class AccountingDepartment extends Department {
         }
         throw new Error("No report found!");
     }
+    describe() {
+        console.log("Accounting Department - ID: " + this.id);
+    }
     //setter
     set setLastReport(value) {
         if (value) {
@@ -62,6 +74,13 @@ class AccountingDepartment extends Department {
             return;
         }
         throw new Error("Please pass in a valid value!");
+    }
+    //Singleton pattern
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        return new AccountingDepartment("Acc-101", []);
     }
     addReport(text) {
         this.reports.push(text);
@@ -78,13 +97,18 @@ class AccountingDepartment extends Department {
         this.employees.push(employeeName);
     }
 }
-const accountsDept = new AccountingDepartment("1A3", ["report1", "report2"]); //[] is the reports
+// const accountsDept = new AccountingDepartment("1A3", ["report1", "report2"]); //[] is the reports
+const accountsDept = AccountingDepartment.getInstance();
+const accountsDept2 = AccountingDepartment.getInstance();
+console.log("=> ", accountsDept, accountsDept2);
 accountsDept.addReport("report3");
-accountsDept.getReports();
+// accountsDept.getReports();
 accountsDept.describe();
-console.log(accountsDept);
+// console.log(accountsDept);
 accountsDept.addEmployee("Max");
-accountsDept.printEmployeeInfo();
+// accountsDept.printEmployeeInfo();
 accountsDept.setLastReport = "report4 latest one";
-console.log(accountsDept.getLastReport);
+// console.log(accountsDept.getLastReport);
 //Static methods and properties
+//creating an employee
+const emp1 = Department.createEmployee("Max");
--- ClassesInterfaces/src/app.ts
+++ ClassesInterfaces/src/app.ts
@@ -1,20 +1,30 @@
 //Class
-class Department {
+abstract class Department {
   //   private id: string;
   //   public name: string;
 
   //private field
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  //static field
+  static year = 2023;
+
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id;
     // this.name = n;
+    // year = 2024;
   }
 
-  //DUMMY PARAMETER
-  describe(this: Department) {
-    console.log(`Department details: ${this.id} ${this.name}`);
+  //static method
+  static createEmployee(name: string) {
+    return { name: name };
   }
+
+  abstract describe(): void; //abstract method
+
+  //DUMMY PARAMETER
+  // console.log(`Department details: ${this.id} ${this.name}`);
+  // }
 
   addEmployee(employee: string) {
     //some validation etc
@@ -35,27 +45,32 @@ class ITDepartment extends Department {
     super(id, "IT"); //super must be called first, it'll call the constructor of the base class
     this.admins = admins;
   }
+
+  describe(): void {
+    console.log("IT Department - ID: " + this.id);
+  }
 }
 
-const accountsDepartment = new Department("1A1", "Accounts");
-console.log(accountsDepartment);
-accountsDepartment.addEmployee("Max");
-accountsDepartment.addEmployee("Maxi");
+// const accountsDepartment = new Department("1A1", "Accounts");
+// console.log(accountsDepartment);
+// accountsDepartment.addEmployee("Max");
+// accountsDepartment.addEmployee("Maxi");
 
 // accountsDepartment.employees.push("Anna");
 
-accountsDepartment.describe();
+// accountsDepartment.describe();
 
 // const acCopy = { name: "Dummy", describe: accountsDepartment.describe };
 // acCopy.describe();
 
-accountsDepartment.printEmployeeInfo();
+// accountsDepartment.printEmployeeInfo();
 
 const ItDept1 = new ITDepartment("1A2", ["Admin_01"]);
 ItDept1.describe();
 
 class AccountingDepartment extends Department {
   private lastReport: string;
+  private static instance: AccountingDepartment;
 
   //getter
   get getLastReport() {
@@ -63,6 +78,10 @@ class AccountingDepartment extends Department {
       return this.lastReport;
     }
     throw new Error("No report found!");
+  }
+
+  describe(): void {
+    console.log("Accounting Department - ID: " + this.id);
   }
 
   //setter
@@ -74,9 +93,18 @@ class AccountingDepartment extends Department {
     throw new Error("Please pass in a valid value!");
   }
 
-  constructor(id: string, private reports: string[]) {
+  private constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
     this.lastReport = reports[0];
+  }
+
+  //Singleton pattern
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+
+    return new AccountingDepartment("Acc-101", []);
   }
 
   addReport(text: string) {
@@ -97,16 +125,23 @@ class AccountingDepartment extends Department {
   }
 }
 
-const accountsDept = new AccountingDepartment("1A3", ["report1", "report2"]); //[] is the reports
+// const accountsDept = new AccountingDepartment("1A3", ["report1", "report2"]); //[] is the reports
+const accountsDept = AccountingDepartment.getInstance();
+
+const accountsDept2 = AccountingDepartment.getInstance();
+console.log("=> ", accountsDept, accountsDept2);
+
 accountsDept.addReport("report3");
-accountsDept.getReports();
+// accountsDept.getReports();
 accountsDept.describe();
-console.log(accountsDept);
+// console.log(accountsDept);
 
 accountsDept.addEmployee("Max");
-accountsDept.printEmployeeInfo();
+// accountsDept.printEmployeeInfo();
 
 accountsDept.setLastReport = "report4 latest one";
-console.log(accountsDept.getLastReport);
+// console.log(accountsDept.getLastReport);
 
 //Static methods and properties
+//creating an employee
+const emp1 = Department.createEmployee("Max");
