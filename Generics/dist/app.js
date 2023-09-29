e230ece2e4d2033a4b701c109f37fab08c0db221
Gaurav <gauravsomani52750@gmail.com>
2023-09-29 17:54:37 +0530

    generic classes

--- Generics/dist/app.js
+++ Generics/dist/app.js
@@ -23,3 +23,35 @@ function countAndDescribe(element) {
     return [element, description];
 }
 console.log(countAndDescribe(["Sports", "Cooking"]));
+function extract(obj, key) {
+    return obj[key];
+}
+console.log(extract({ age: 20, name: "Gaurav" }, "name"));
+//* Generic Classes
+//hence, this class will only work for primitive types
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(data) {
+        this.data.push(data);
+    }
+    removeItem(data) {
+        this.data.splice(this.data.indexOf(data), 1);
+    }
+    getItems() {
+        return this.data;
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Gaurav");
+textStorage.addItem("Kanak");
+textStorage.addItem("Sar");
+// textStorage.addItem(11);
+textStorage.removeItem("Sar");
+console.log(textStorage.getItems());
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Gaurav" });
+// objStorage.addItem({ name: "Kanak" });
+// // objStorage.removeItem({ name: "Gaurav" }); //does not work for objects
+// console.log(objStorage.getItems());
--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -35,3 +35,45 @@ function countAndDescribe<T extends Lengthy>(element: T) {
 }
 
 console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extract<T, U extends keyof T>(obj: T, key: U) {
+  return obj[key];
+}
+
+console.log(extract({ age: 20, name: "Gaurav" }, "name"));
+
+//* Generic Classes
+
+//hence, this class will only work for primitive types
+class DataStorage<T extends number | string | boolean> {
+  private data: T[] = [];
+
+  addItem(data: T) {
+    this.data.push(data);
+  }
+
+  removeItem(data: T) {
+    this.data.splice(this.data.indexOf(data), 1);
+  }
+
+  getItems() {
+    return this.data;
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Gaurav");
+textStorage.addItem("Kanak");
+textStorage.addItem("Sar");
+// textStorage.addItem(11);
+
+textStorage.removeItem("Sar");
+
+console.log(textStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Gaurav" });
+// objStorage.addItem({ name: "Kanak" });
+// // objStorage.removeItem({ name: "Gaurav" }); //does not work for objects
+
+// console.log(objStorage.getItems());
