27b7d22e896b28221456a8b7156ed38217512e66
Gaurav <gauravsomani52750@gmail.com>
2023-09-30 10:37:30 +0530

    element selection, rendering form

--- DragAndDrop/dist/app.js
+++ DragAndDrop/dist/app.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("Project time!");
+class ProjectInput {
+    constructor() {
+        console.log("constructor called");
+        //reference to both the elements
+        this.templateElement = document.getElementById("project-input");
+        this.divElement = document.getElementById("app");
+        //this is the copy of the template
+        const importNode = document.importNode(this.templateElement.content, true);
+        //* this is the 'form' element, which is the first child of the template
+        this.element = importNode.firstElementChild;
+        console.log(this.element);
+        this.setDiv();
+    }
+    setDiv() {
+        //attach the form element to the div element
+        this.divElement.appendChild(this.element);
+    }
+}
+const projectInput = new ProjectInput();
--- DragAndDrop/src/app.ts
+++ DragAndDrop/src/app.ts
@@ -1,1 +1,32 @@
-console.log("Project time!");
+class ProjectInput {
+  templateElement: HTMLTemplateElement; //template
+  divElement: HTMLDivElement; //jaha par hume ye template dalna hai
+  element: HTMLFormElement;
+
+  constructor() {
+    console.log("constructor called");
+
+    //reference to both the elements
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+
+    this.divElement = document.getElementById("app")! as HTMLDivElement;
+
+    //this is the copy of the template
+    const importNode = document.importNode(this.templateElement.content, true);
+
+    //* this is the 'form' element, which is the first child of the template
+    this.element = importNode.firstElementChild as HTMLFormElement;
+    console.log(this.element);
+
+    this.setDiv();
+  }
+
+  setDiv() {
+    //attach the form element to the div element
+    this.divElement.appendChild(this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
