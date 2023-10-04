f03d30a3db8cbe4a260314b93c3f0912129a258d
Gaurav <gauravsomani52750@gmail.com>
2023-10-04 11:21:33 +0530

    setup webpack for development

--- DragAndDrop/webpack.config.js
+++ DragAndDrop/webpack.config.js
@@ -1,10 +1,40 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
+
   entry: "./src/app.ts",
 
   output: {
     filename: "bundle.js", //single js file which will be generated in the end
     path: path.resolve(__dirname, "dist"), //where output file will be generated
+    publicPath: "/dist/", //this configuration is required to tell webpack-dev-server where to look for the bundle.js file
   },
+
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+
+  devtool: "inline-source-map", //to debug ts files in browser
+
+  module: {
+    //to tell webpack how to handle different files
+    rules: [
+      {
+        test: /\.ts?$/, //webpack will check whether its a .ts file
+        use: "ts-loader", // if yes then use ts-loader to compile it - ts-loader will use typescript compiler and tsconfig.json to compile it
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+
+  stats: { warnings: false },
 };
